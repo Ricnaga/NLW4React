68bb673e430320840d8088990810fcf0a1d3f761
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-04 14:17:20 -0300

    add global pages | styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { GetServerSideProps } from "next";
+import Head from 'next/head'
+import { CompletedChallenge } from "../components/CompletedChallenge";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from "../contexts/ChallengeContext";
+import styles from '../styles/pages/Home.module.css'
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengeCompleted: number;
+}
+
+export default function Home(props:HomeProps) {
+  return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengeCompleted={props.challengeCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenge />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengeCompleted } = ctx.req.cookies
+  return {
+    props: {
+      level:Number(level),
+      currentExperience:Number(currentExperience),
+      challengeCompleted:Number(challengeCompleted),
+    }
+  }
+}
